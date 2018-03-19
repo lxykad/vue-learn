9afde7e0616b7b22cb1532fc2613215e1453149b
1 <a@1deiMac-4.local>
2018-03-19 14:26:51 +0800

    add router test

---
+++ src/components/home/home.vue
@@ -0,0 +1,95 @@
+<!-- html-->
+<template>
+  <div id="container">
+    <button v-on:click="click">{{a}}</button>
+
+    <router-link to="/three">
+      <div id="text">55555</div>
+    </router-link>
+
+    <router-link to="/hello"> 带参跳转 </router-link>
+    <button @click="go">go</button>
+    <transition name="slide">
+      <router-view></router-view>
+    </transition>
+
+  </div>
+
+</template>
+
+<!--js-->
+<script>
+  export default {
+
+    data() {
+      return {
+        a: 1
+      }
+    },
+    methods: {
+      click() {
+        this.a++
+      },
+      go() {
+        console.log('go')
+        this.$router.push('/hello')
+      }
+    },
+    watch: {
+      a: function (value, oldValue) {
+
+      }
+    },
+
+    beforeCreate: function () {
+
+      console.log('home before created')
+    },
+
+    /**
+     * 实例被创建之后执行代码
+     */
+    created: function () {
+
+      console.log('home created')
+    },
+
+    mounted: function () {
+      console.log('home mounted')
+    },
+
+    destoryed: function () {
+      console.log('home destoryed')
+    },
+
+  }
+
+</script>
+
+<!--css-->
+<style>
+
+  #container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+  }
+
+  button {
+    padding: 5px 10px;
+    margin-top: 10px;
+  }
+
+  #text {
+    margin-top: 10px;
+  }
+
+  router-view {
+    display: flex;
+    background-color: aqua;
+    width: 100%;
+
+  }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,23 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import ThreeD from '@/components/ThreeD'
+import Home from '../components/home/home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'ThreeD',
+      path: '/three',
       component: ThreeD
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/hello',
+      component: HelloWorld
     }
   ]
 })
